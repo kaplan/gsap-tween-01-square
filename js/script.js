1bba24b383457cfcb1760fd7e3d61908e3b87569
kaplan <kaplan@workalicious.com>
2012-12-26 08:38:57 -0600

    Changing colors using SASS/Compass
    Started using SASS and Compass for the style. Cleaned up the Javascript file a bit. Changed the color from Orange to Yellow.
    
    Signed-off-by: kaplan <kaplan@workalicious.com>

--- js/script.js
+++ js/script.js
@@ -20,6 +20,12 @@ function initAnimation() {
 	var txtEl = txt[0];	
 	var squareTxt = txt[0].firstChild.nodeValue;
 	var revSquareTextElem = squareTxt.split("").reverse().join("");
+	var colorSet = {
+		bright: "#ffcb08",
+		light: "#efefef",
+		dark: "#3b3b3b"
+		};
+	console.log(colorSet.bright);
 
 	console.log("----> " + txt.length);
 	console.log("----> " + txt[0].firstChild.nodeValue);
@@ -41,14 +47,14 @@ function initAnimation() {
 	// GSAP tweens to run based on delayed timing
 	TweenMax.to(box, 1, {css:{left:xc}, ease:Back.easeInOut});
 	TweenMax.to(box, 1, {css:{width:400, height:400, x:"-100", y:"-100"}, ease:Elastic.easeIn, delay:1});
-	TweenMax.to(box, 0.5, {css:{rotation:360, backgroundColor:"#3b3b3b"}, ease:Quad.easeInOut, delay:1.5});
-	TweenMax.to(document.body, 1, {css:{backgroundColor:"#ff6600"}, delay:2.0});
+	TweenMax.to(box, 0.5, {css:{rotation:360, backgroundColor:colorSet.dark}, ease:Quad.easeInOut, delay:1.5});
+	TweenMax.to(document.body, 1, {css:{backgroundColor:colorSet.bright}, delay:2.0});
 	TweenMax.delayedCall(2, animateSquare);
 
 	// GSAP timeline tween created and paused and set. this will play based on a delayed call
 	tl = new TimelineMax({paused:true});
 	tl.to(txtEl, 2, {css:{marginTop:-40, color:"#fff"}, ease:Elastic.easeInOut});
-	tl.to(txtEl, 1, {css:{color:"#ff6600"}, ease:Quad.easeOut});
+	tl.to(txtEl, 1, {css:{color:colorSet.bright}, ease:Quad.easeOut});
 	tl.to(sq, .25, {onStart:revSquareText, css:{rotation:360}, ease:Quad.easeOut}, -1);
 
 	// show the little square with a GSAP delayed function call
@@ -56,7 +62,7 @@ function initAnimation() {
 		console.log("YO!");
 		sq.style.display = "block";
 		TweenMax.from(sq, 1, {css:{top:-100}, ease:Elastic.easeOut});
-		TweenMax.to(sq, 1, {css:{backgroundColor:"#ff6600"}, ease:Quad.easeOut, delay:1});
+		TweenMax.to(sq, 1, {css:{backgroundColor:colorSet.bright}, ease:Quad.easeOut, delay:1});
 		tl.play();
 	}
 	
